5bf9f0f8f4218987342913a03c75a52dc0fd99de
akshat-vv <akshatvijay14@gmail.com>
2023-12-14 00:31:32 +0530

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
 
-const Banner = () => {
+const Banner = ({ banner }) => {
+
+
+
   return (
-    <div>Banner</div>
+    <>
+      {
+        banner.map((banner) => {
+          const { imageId, action } = banner;
+          const { text } = action;
+          return <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${imageId}`} alt="" className="h-40 mix-blend-multiply" />
+        })
+      }
+    </>
   )
 }
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -15,20 +15,28 @@ const Body = () => {
   const  getRestaurants = async (API_URL) => {
     let response =  await fetch(API_URL);
     let data = await response.json();
-    setAllRestaurants(data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants.slice(0,18));
+    setAllRestaurants(data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants.slice(0, 18));
+    setBanner(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.info.slice(0, 8));
   }
 
   return (
-    
-    <div className='p-4 flex flex-wrap justify-between gap-3'>
-
-      { allRestaurants.length === 0 ? <Shimmer/> :
-        allRestaurants.map((restaurant)=>{
-           return <RestaurantCard {...restaurant.info}/>
-        })
+    <div className='flex flex-col p-4'>
+      {banner.length > 0 &&
+        < div className=' mb-4 rounded-m flex justify-around' >
+          <Banner banner={banner} />
+        </div>
       }
-      
+      <div className='flex flex-wrap justify-between gap-3'>
+
+        {allRestaurants?.length === 0 ? <Shimmer /> :
+          allRestaurants?.map((restaurant) => {
+            return <RestaurantCard {...restaurant.info} />
+          })
+        }
+
+      </div>
     </div>
+
   )
 }
 
